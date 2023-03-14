6c16cc9516674b3651a6bfeaa4fe3589538b4440
Mick Hansen <maker@mhansen.io>
2023-03-14 13:27:24 +0100

    remove JsonProperty from SignatureEvidenceProvider

--- Criipto.Signatures/SignatureEvidenceProvider.cs
+++ Criipto.Signatures/SignatureEvidenceProvider.cs
@@ -1,16 +1,11 @@
 #pragma warning disable CS8618
-#pragma warning disable CA1507
 #pragma warning disable CA1707
 
-using System.ComponentModel.DataAnnotations;
-using Newtonsoft.Json;
 namespace Criipto.Signatures;
 
 public class SignatureEvidenceProvider
 {
-    [JsonProperty("__typename")]
     public string __typename { get; set; }
 
-    [JsonProperty("id")]
     public string id { get; set; }
 }
